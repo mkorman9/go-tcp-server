a16a3389c5179517dca63fe83506b0a881f551be
Michał Korman <michal.korman@icloud.com>
2022-11-04 20:42:07 +0100

    use bytes.Join

--- connection_handler.go
+++ connection_handler.go
@@ -31,24 +31,31 @@ func (c *connectionHandler) OnAccept(socket *tcputil.ClientSocket) {
 
 	c.readBuffersMutex.Lock()
 	defer c.readBuffersMutex.Unlock()
-	c.readBuffers[socket] = make([]byte, 0)
+	c.readBuffers[socket] = nil
 }
 
 func (c *connectionHandler) OnRead(socket *tcputil.ClientSocket, data []byte) {
-	b := c.readBuffers[socket]
-	b = append(b, data...)
+	if len(c.readBuffers[socket])+len(data) > 8196 {
+		c.readBuffers[socket] = nil
+		return
+	}
+
+	buffer := bytes.Join([][]byte{
+		c.readBuffers[socket],
+		data,
+	}, nil)
 
 	for {
-		packetData, other, ok := bytes.Cut(b, []byte{'\n'})
+		packetData, other, ok := bytes.Cut(buffer, []byte{'\n'})
 		if !ok {
 			break
 		}
 
 		c.onPacket(socket, packetData)
-		b = other
+		buffer = other
 	}
 
-	c.readBuffers[socket] = b
+	c.readBuffers[socket] = buffer
 }
 
 func (c *connectionHandler) onPacket(socket *tcputil.ClientSocket, packetData []byte) {
